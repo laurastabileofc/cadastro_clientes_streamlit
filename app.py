e79c02134a2b1044e257afba463f4c39a3b087fa
laurastabileofc <laurastabileofc@gmail.com>
2026-03-14 11:03:32 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('Cadastro de clientes.')
+
+nome= st.text_input('Digite o nome do cliente')
+endereco= st.text_input('Digite o endereço')
+dt_nasc= st.date_input('Escolha a data de nascimento')
+tipo_cliente= st.selectbox('Tipo de cliente', ['Pessoa Física', 'Pessoa Jurídica'])
+cadastrar= st.button('Cadastrar cliente')
+
+if cadastrar:
+   with open('Clientes.csv', 'a', encoding='utf8') as arquivo:
+      arquivo.write(f'{nome}, {endereco}, {dt_nasc}, {tipo_cliente} \n') 
+      st.success('Cadastro concluído com sucesso.')
+
+#st.texte_input cria uma caixa de texto na web
